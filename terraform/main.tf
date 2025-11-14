557befe054f4894c5c08e20bf73bec37915117af
jayesh <jayeshkarad1995@gmail.com>
2025-11-14 19:48:14 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,32 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks.token
   load_config_file       = false
 }
+
+# ============================================================
+# Security group rules for public access to the Load Balancer
+# ============================================================
+# We are using the *additional* security group:
+#   sg-068b519bad73771fa
+# Do NOT open the cluster security group (API server) to 0.0.0.0/0.
+
+# Allow HTTP (port 80) from anywhere to the Load Balancer SG
+resource "aws_security_group_rule" "lb_public_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = "sg-068b519bad73771fa"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow public HTTP access to Load Balancer"
+}
+
+# Allow HTTPS (port 443) from anywhere to the Load Balancer SG
+resource "aws_security_group_rule" "lb_public_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = "sg-068b519bad73771fa"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow public HTTPS access to Load Balancer"
+}
